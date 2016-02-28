4ebcd19378d06010dcd455c9432ed474db31f2e2
SmBe19 <bsgame27@gmail.com>
2016-02-28 15:31:03 +0100

    Implement automatic image placing

--- InDesign/LayoutReveil.js
+++ InDesign/LayoutReveil.js
@@ -9,6 +9,12 @@ var MASTERSPREAD_INDEX = 0;
 var MASTERSPREAD_INDEXES_LEFT = [0, 1, 2, 3];
 var MASTERSPREAD_INDEXES_RIGHT = [0, 1, 2, 3];
 var END_OF_SENTENCE = [".", "?", "!"];
+var IMAGE_REGEX = /\[\[([^|\[\]]+)\|([^\[\]]*)\]\]/g
+var IMAGE_WIDTH_PT = 181.085669;
+var IMAGE_HEIGHT_PT = 181.085669;
+var IMAGE_WIDTH_MM = 63.883;
+var IMAGE_HEIGHT_MM = 63.883;
+var IMAGES_PER_LINE = 3;
 
 var logfile;
 
@@ -105,6 +111,22 @@ function getCharacterStyleByName(name){
     }
 }
 
+function getLibraryByName(name){
+    for(var i = 0; i < app.libraries.length; i++){
+        if(app.libraries.item(0).name == name){
+            return app.libraries.item(0);
+        }
+    }
+}
+
+function getLibraryAssetByName(library, name){
+    for(var i = 0; i < library.assets.length; i++){
+        if(library.assets.item(i).name == name){
+            return library.assets.item(i);
+        }
+    }
+}
+
 function strEndsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
@@ -124,7 +146,7 @@ function isEndOfLead(paragraph){
     }
     var wordsUntilPoint = 0;
     for(; wordsUntilPoint < paragraph.words.length && !strEndsWithList(paragraph.words[paragraph.words.length - 1 - wordsUntilPoint].contents, END_OF_SENTENCE); wordsUntilPoint++);
-    return wordsUntilPoint < 5;
+    return wordsUntilPoint > 0 && wordsUntilPoint < 6;
 }
 
 function removeBlankParagraphs(story){
@@ -134,7 +156,6 @@ function removeBlankParagraphs(story){
         }
     }
 }
-    
 
 // layout the pages according to the given layout
 // the layout to use
@@ -147,6 +168,8 @@ function layoutPages(layout){
             continue;
         }
         log("layout " + article.filename);
+        
+        // override pages
         var startpage = document.pages[article.startpage - 1];
         overridePage(startpage);
         for(var aPage = article.startpage; aPage < article.startpage - 1 + article.pagecount; aPage++){
@@ -155,15 +178,16 @@ function layoutPages(layout){
             for(var j = 0; j < 2; j++){
                 page.textFrames[0].remove();
             }
-            page.textFrames[0].previousTextFrame = document.pages[aPage - 1].textFrames[MASTERSPREAD_INDEXES_LEFT.length - 1];
+            page.textFrames[0].previousTextFrame = document.pages[aPage - 1].textFrames[document.pages[aPage - 1].textFrames.length - 1];
         }
     
+        // place text
         var oldInteractionPrefs = app.scriptPreferences.userInteractionLevel;
         app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
         startpage.textFrames[0].place(article.file);
         app.scriptPreferences.userInteractionLevel = oldInteractionPrefs;
         
-        
+        // format title and lead
         story = startpage.textFrames[0].parentStory;
         removeBlankParagraphs (story);
         story.paragraphs[0].appliedParagraphStyle = getParagraphStyleByName("Titel");
@@ -173,18 +197,54 @@ function layoutPages(layout){
         }
     
         if (ap < story.paragraphs.length){
+            // format author
             story.paragraphs[ap].appliedParagraphStyle = getParagraphStyleByName("Leadtext");
             for(var aw = story.paragraphs[ap].words.length - 1; aw >= 0 && !strEndsWithList(story.paragraphs[ap].words[aw].contents, END_OF_SENTENCE); aw--){
                 story.paragraphs[ap].words[aw].appliedCharacterStyle = getCharacterStyleByName ("Autor");
             }
             ap++;
             
+            // format fliesstext
             for(; ap < story.paragraphs.length; ap++){
                 story.paragraphs[ap].appliedParagraphStyle = getParagraphStyleByName("Fliesstext");   
             }
         } else {
             log(article.filename + " is missing a lead text");
         }
+    
+        // place images
+        var lastImagePos = 0;
+        var placeImage = function(match, image, description){
+            imageFile = new File(layout.rootpath.absoluteURI + "/" + image);
+            if(!imageFile.exists){
+                log("Image " + imageFile.absoluteURI + " does not exist");
+            } else {
+                log("Place image " + image + (description.length > 0 ? " with description" : ""));
+                var moveBy = [(lastImagePos % IMAGES_PER_LINE) * IMAGE_WIDTH_MM, Math.floor(lastImagePos / IMAGES_PER_LINE) * IMAGE_HEIGHT_MM];
+                var image = document.pages[article.startpage-1].place(imageFile)[0];
+                var imageFrame = image.parent;
+                // Resize image, using height*2 such that the width is correct after fitting
+                imageFrame.resize(CoordinateSpaces.INNER_COORDINATES, AnchorPoint.TOP_LEFT_ANCHOR,
+                    ResizeMethods.REPLACING_CURRENT_DIMENSIONS_WITH, [IMAGE_WIDTH_PT, IMAGE_HEIGHT_PT*2]);
+                imageFrame.fit(FitOptions.PROPORTIONALLY);
+                imageFrame.fit(FitOptions.FRAME_TO_CONTENT);
+                imageFrame.move([imageFrame.geometricBounds[1], 0]);
+                imageFrame.move(undefined, moveBy);
+                
+                if(description.length > 0){
+                    var asset = getLibraryAssetByName(getLibraryByName ("reveil_bibliothek.indl"), "Bildunterschrift");
+                    var placed = asset.placeAsset(document)[0];
+                    placed.contents = description;
+                    placed.move(document.pages[article.startpage-1]);
+                    placed.move([imageFrame.geometricBounds[1], imageFrame.geometricBounds[2]]);
+                }
+                
+                lastImagePos++;
+            }
+            return "";
+        };
+        //story.contents = story.contents.replace(IMAGE_REGEX, placeImage);
+        story.contents.replace(IMAGE_REGEX, placeImage);
     }
 }
 
